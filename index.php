03e8f9b5b25f17e24b74858c4c582581f91ac543
Unknown <svenjanssen12321@gmail.com>
2019-02-18 11:22:06 +0100

    Database communication
    Build app ✔️

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <title>Greenhouse flexwerk monitor</title>
 </head>
 <body>
-    <p>test</p>
-
+    <h1 id="bigOne"></h1>
     
     <script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
     <script>
@@ -22,6 +21,10 @@
         messagingSenderId: "394804551221"
     };
     firebase.initializeApp(config);
+
+    var bigOne = document.getElementById('bigOne');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigOne.innerText = snap.val());
     </script>
 </body>
 </html>
