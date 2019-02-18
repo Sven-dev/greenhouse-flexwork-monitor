4375c7597a1d27dd4231714e1fc279e9deedafea
Unknown <svenjanssen12321@gmail.com>
2019-02-18 11:37:01 +0100

    Experimenting with .js syntax

---
+++ backend/dbcon.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAFpQy1ZMeGA4XFtxIHWHI2A_EAAEGRs2E",
+    authDomain: "greenhouse-flexwork-monitor.firebaseapp.com",
+    databaseURL: "https://greenhouse-flexwork-monitor.firebaseio.com",
+    projectId: "greenhouse-flexwork-monitor",
+    storageBucket: "greenhouse-flexwork-monitor.appspot.com",
+    messagingSenderId: "394804551221"
+};
+firebase.initializeApp(config);
