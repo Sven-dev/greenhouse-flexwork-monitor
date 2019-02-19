2a36ecd602ef5fe395643a459eca809de5cd01fc
Sharona <sharonasteevens@gmail.com>
2019-02-19 10:32:19 +0100

    Bootstrap 4 added
    css, header.php added

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Greenhouse flexwerk monitor</title>
-</head>
-<body>
-    <h1 id="bigOne"></h1>
+<?php
+    include 'header.php';
+?>    
 
-    <h2 id="bigTwo"></h2>
-    <button onclick="changeData('Object', 'Henlo!')">Hello!</button>
-    <button onclick="changeData('Object', 'Byebye!')">ByeBye!</button>
+<div class="jumbotron text-center">
+        <h1 id="bigOne"></h1>
+        <h2 id="bigTwo"></h2>
+        <button class="btn btn-primary" onclick="changeData('Object', 'Hello!')">Hello!</button>
+        <button class="btn btn-success" onclick="changeData('Object', 'Byebye!')">ByeBye!</button>
+</div>
+
+    <div class="container">
+       
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    
+    </div>
     
     <script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
     <script src="backend/dbcon.js"></script>
