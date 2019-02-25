837b8d693ad234db8c5a17212485798640c67caf
Diane <laciepark300323@hotmail.com>
2019-02-25 15:57:50 +0100

    Navigation bar
    Added Navbar code and styling

--- header.php
+++ header.php
@@ -16,13 +16,32 @@
 </head>
 
 <body onLoad="logInCheck()">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<ul>Home</ul>
-				<ul>Home</ul>
-				<ul>Home</ul>
-				<ul>Home</ul>
-			</div>
-		</div>	
+
+	<div class="navigation-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<ul>
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Map</a></li>
+						<li><img src="image/logo.png" alt="Greenhouse Logo" width="50px" height="50px"></li>
+						<li><a href="#">Game</a></li>
+						<li><a href="#">People</a></li>
+					</ul>
+				</div>
+			</div>	
+		</div>
+	</div>
+
+	<div class="profile-bar offset-10">
+
+		<a class="dropdown-toggle" href="#" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome Alies</a>
+
+		<div class="dropdown-menu">
+			<ul class="list-unstyled">
+		        <li class="dropdown-item border-bottom"><a href="#">Edit Profile</a></li>
+		        <li class="dropdown-item border-bottom"><a href="#">Settings</a></li>
+		        <li class="dropdown-item"><a href="#">Log Out</a></li>
+		    </ul>
+		</div>
 	</div>
