46047a4144370ce362f0ba9d61fd9a350752e44c
Unknown <svenjanssen12321@gmail.com>
2019-02-26 14:08:20 +0100

    Changed searchresults list ordering

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 						<input id="search-colleague" type="text" name="search" placeholder="Search...">
 					</div>
 					<!-- <button class="btns-primary" type="button" name="find-colleague">Find</button> -->
-					<div id="search-results">
-					</div>
+					<div id="search-results"></div>
 				</form>
 			</div>
 		</div>		
